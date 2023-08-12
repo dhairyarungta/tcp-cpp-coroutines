cdde35e13d759e741c2bb4c9e05cd8ea321ec766
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-13 02:21:32 +0800

    buffer implementation updates

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -18,6 +18,20 @@ ByteBuffer::~ByteBuffer()
     delete[] this->data;
 }
 
+long long ByteBuffer::get_index(long long p)
+{
+    return p % max_capacity;
+}
+
+bool ByteBuffer::has_wrap_around()
+{
+    long long quotient_w = write_p/max_capacity;
+    long long quotient_r = read_p/max_capacity;
+    return quotient_w>quotient_r && read_p > (quotient_r*max_capacity);
+    
+}
+
+
 long long ByteBuffer::get_total_capacity()
 {
     return max_capacity- get_total_remaining();
@@ -26,4 +40,40 @@ long long ByteBuffer::get_total_capacity()
 long long ByteBuffer::get_total_remaining()
 {
     return write_p-read_p;
+}
+
+
+void ByteBuffer::increment_read_pointer(long long size)
+{
+    if(size>get_total_capacity())
+    {
+        throw std::runtime_error("ByteBuffer::increment_read_pointer error: More than capacity");
+
+    }
+    write_p+=size;
+}
+
+void ByteBuffer::read(std::byte* dest, long long size)
+{
+
+}
+
+void ByteBuffer::write(std::byte* srcdest, long long)
+{
+
+}
+
+void ByteBuffer::increment_read_pointer(long long size )
+{
+
+}
+
+IOChunk ByteBuffer::get_write_chunk()
+{
+
+}
+
+IOChunk ByteBuffer::get_read_chunk()
+{
+    
 }

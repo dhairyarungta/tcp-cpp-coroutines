e5f98d19bcf19d7653a17b3b2122cc67da6c11ba
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-14 02:32:06 +0800

    update

--- extra/coroutine_thread_pool.cpp
+++ extra/coroutine_thread_pool.cpp
@@ -1,110 +1,125 @@
 #include <iostream>
+#include <thread>
 #include <vector>
 #include <queue>
-#include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <functional>
 #include <coroutine>
 
-class ThreadPool
-{
-public :
-    ThreadPool(size_t numThreads): stop(false)
-    {
-        workers.reserver(numThreads);
-        for(size_t i =0 ;i<numThreads;++i)
-        {
-            workers.emplace_back([this]{WorkerThread();});
+template <typename T>
+class Future {
+public:
+    struct Promise {
+        Promise() : value(nullptr) {}
+        Future get_return_object() {
+            return Future{std::coroutine_handle<Promise>::from_promise(*this)};
+        }
+        std::suspend_never initial_suspend() { return {}; }
+        std::suspend_never final_suspend() noexcept { return {}; }
+        void return_value(const T& val) {
+            value = new T(val);
+        }
+        void unhandled_exception() { std::terminate(); }
+
+        T* value;
+    };
+
+    Future(std::coroutine_handle<Promise> handle) : handle(handle) {}
+    ~Future() {
+        if (handle) handle.destroy();
+    }
+    
+    T get() {
+        return *(handle.promise().value);
+    }
+
+private:
+    std::coroutine_handle<Promise> handle;
+};
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads) : stop(false) {
+        for (size_t i = 0; i < numThreads; ++i) {
+            threads.emplace_back([this] { WorkerThread(); });
         }
     }
 
-    ~ThreadPool()
-    {
+    ~ThreadPool() {
         {
-            std::unique_lock<std::mutex>lock(mutex_);
+            std::unique_lock<std::mutex> lock(mutex);
             stop = true;
         }
-        condition_.notify_all();
-        for (std::thread& worker:workers)
-        {
-            worker.join();
+        condition.notify_all();
+        for (auto& thread : threads) {
+            thread.join();
         }
     }
 
-    template<typename Func>
-    auto Enqueue(Func func)
-    {
+    template <typename Func>
+    Future<void> Enqueue(Func func) {
         using ReturnType = std::invoke_result_t<Func>;
-        struct Task 
-        {
+
+        struct Task {
             Func func;
             std::coroutine_handle<> coroutine;
         };
 
-        std::coroutine_handle<> coroutine= std::coroutine_handle<>::from_address(nullptr);
-        auto task = std::make_unique<Task> (Task{std::move(func), coroutine});
+        auto task = std::make_unique<Task>(Task{std::move(func), nullptr});
         auto future = task->coroutine.promise().get_return_object();
+
         {
-            std::unique_lock<std::mutex>lock(mutex_);
+            std::unique_lock<std::mutex> lock(mutex);
             tasks.push(std::move(task));
-
         }
-        condition_.notify_all();
+
+        condition.notify_one();
+
         return future;
-
     }
-private :
-    void WorkerThread()
-    {
-        while(true)
-        {
-            std::unique_ptr<Task> task;
-            {
-                std::unique_lock<std::mutex> lock(mutex_);
-                condition_.wait(lock, [this]{return stop||!tasks.empty();});
-                if(stop&& tasks.empty())
-                {
-                    return;
-                }
-                task = std::move(task.front());
-                tasks.pop();
 
-            }
+private:
+    void WorkerThread() {
+        while (true) {
+            std::unique_lock<std::mutex> lock(mutex);
+            condition.wait(lock, [this] { return stop || !tasks.empty(); });
+            if (stop) return;
+
+            auto task = std::move(tasks.front());
+            tasks.pop();
+
+            lock.unlock();
+
+            task->coroutine = std::coroutine_handle<>::from_address(nullptr);
             task->func();
             task->coroutine.resume();
+
+            lock.lock();
         }
     }
 
-    using Task = struct {std::function<void()>func, std::coroutine_handle<> coroutine;};
-    std::vector<std::thread>workers;
-    std::queue<std::unique_ptr<Task>>tasks;
-    std::mutex mutex_;
-    std::condition_variable condition_;
+private:
+    std::vector<std::thread> threads;
+    std::queue<std::unique_ptr<Task>> tasks;
+    std::mutex mutex;
+    std::condition_variable condition;
     bool stop;
 };
 
-int main()
-{
+int main() {
     ThreadPool threadPool(4);
 
-    auto asyncTask = [](int id)->std::coroutine_handle<>
-    {
-        std::cout<<"Async Task "<<id<<started<<std::endl;
-        co_await std::suspend_always();
-        std::cout<<<"Async task "<<id<< "completed"<<std::endl;
-    };
-    std::vector<std::coroutine_handle<>> coroutines;
-    for (int i =0;i<10;++i)
-    {
-        coroutines.push_back(threadPool.Enqueue([i,asyncTask]()mutable{
-            asyncTask(i);
-        }));
-    }
+    auto task1 = threadPool.Enqueue([]() -> Future<int> {
+        co_return 42;
+    });
 
-    for (auto coroutine : coroutines)
-    {
-        coroutine.resume();
-    }
+    auto task2 = threadPool.Enqueue([]() -> Future<int> {
+        co_return 73;
+    });
+
+    std::cout << "Task 1 result: " << task1.get() << std::endl;
+    std::cout << "Task 2 result: " << task2.get() << std::endl;
+
     return 0;
-    
 }

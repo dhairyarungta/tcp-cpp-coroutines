89ad2269b72c6f7d1b8b133fc5623ae7d2eb3ccd
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-14 14:43:03 +0800

    server update:

---
+++ src/server.cpp
@@ -0,0 +1,55 @@
+#include "../include/server.h"
+#include "../include/socket.h"
+#include "../include/app.h"
+#include "../include/thread_pool.h"
+#include "../include/monitor.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <memory>
+
+
+Server::Server(short port , ServerApplication& server_app, IoEventMonitor&io_monitor, ThreadPool& thread_pool)
+    :server_socket(port, io_monitor), server_app(server_app), io_monitor(io_monitor), thread_pool(thread_pool)
+{
+    this->market_for_close= false;
+}    
+
+Future Server::run_server_loop()
+{
+    try
+    {
+        while(!market_for_close)
+        {
+
+        }
+    }
+    catch(const std::runtime_error error)
+    {
+        std::cout<<"Error: "<<error.what()<<std::endl;
+
+    }
+    server_socket.close_socket();
+    
+}
+
+void Server::start()
+{
+    thread_pool.run([&]
+    {
+        run_server_loop();
+    });
+}
+
+void Server::shutdown()
+{
+    if(market_for_close.exchange(true))
+    {
+        throw std::runtime_error(std::string("Server::shutdown() error: Server already shutdown"));
+
+    }
+    server_socket.close_socket();
+    server_app.shutdown();
+    io_monitor.shutdown();
+    thread_pool.shutdown();
+}

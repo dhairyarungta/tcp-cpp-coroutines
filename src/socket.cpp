a93810e768293c9b93f166b7ba564e8f3ae5297b
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-08-14 20:52:36 +0800

    updates

--- src/socket.cpp
+++ src/socket.cpp
@@ -35,16 +35,62 @@ void Socket::read_available(std::byte* &dest, long long &size, long long &total_
 
 AwaitableValue<long long> Socket::read(std::byte* dest, long long size, bool read_fully)
 {
+    long long total_read = 0;
+    read_available(dest ,size, total_read);
+    while(size>0)
+    {
+        SocketOperation status = stream.recv_from_socket(input_buffer);
+        read_available(dest ,size, total_read);
+        if(size==0||status==SOCKET_OP_SUCCESS)
+        {
+            continue;
+        }
+        if(status==SOCKET_OP_BLOCK)
+        {
+            co_await io_listener->await_read();
+        }else if(status==SOCKET_OP_CLOSE)
+        {
+            if(read_fully)
+            {
+                throw std::runtime_error("Socket read(): Socket closed");
 
+            }
+            break;
+        }
+
+    }
+    co_return total_read;
 }
+void Socket::skip_available(long long &size, long long &total_skipped)
+{
+    long long buffer_remaining =input_buffer.get_total_remaining();
+    if(buffer_remaining==0)
+    {
+        return;
+    }
+    long long size_to_skip = std::min(size, buffer_remaining);
+    input_buffer.increment_read_pointer(size_to_skip);
+    size-=size_to_skip;
+    total_skipped +=size_to_skip;
+}
+
 AwaitableValue<long long> Socket::skip(long long size, bool skip_fully)
 {
-    long long total_skipped 
+    long long total_skipped =0;
+    
 }
 
 void Socket::write_availble(const std::byte*& src, long long &size)
 {
-
+    long long buffer_capacity = output_buffer.get_total_capacity();
+    if(buffer_capacity==0)
+    {
+        return;
+    }
+    long long size_to_write = std::min(buffer_capacity,size);
+    output_buffer.write(src, size_to_write);
+    src+=size_to_write;
+    size-=size_to_write;
 }
 
 
